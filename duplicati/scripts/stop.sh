8cf2ed481d5247f64123abd9c0d95c18f2bb72e4
Adam Neumann <noizwaves@users.noreply.github.com>
2020-07-26 22:05:08 -0600

    Perform backup of SN

--- duplicati/scripts/stop.sh
+++ duplicati/scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker stop nextcloud nextcloud_mariadb resilio-sync bitwarden_rs freshrss freshrss_mariadb
+docker stop nextcloud nextcloud_mariadb resilio-sync bitwarden_rs freshrss freshrss_mariadb standardnotes_sync standardnotes_mariadb
