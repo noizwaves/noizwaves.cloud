d7beaf53d78107a76751db508e469243b190d57e
Adam Neumann <noizwaves@users.noreply.github.com>
2020-08-22 17:38:40 -0600

    Handle PNGs when syncing

--- resilio-sync/photosync/index.js
+++ resilio-sync/photosync/index.js
@@ -1,8 +1,8 @@
 const chokidar = require('chokidar')
-const { ExifImage } = require('exif')
 const path = require('path')
+const ExifReader = require('exifreader');
 const {mkdir} = require('fs').promises
-const {copyFileSync} = require('fs')
+const {copyFileSync, readFileSync} = require('fs')
 
 const sourceRootPath = process.env.SOURCE_ROOT_PATH
 const destinationRootPath = process.env.DESTINATION_ROOT_PATH
@@ -12,28 +12,48 @@ console.log(`> From source: ${sourceRootPath}`)
 console.log(`> To Destination: ${destinationRootPath}`)
 console.log('')
 
+// Returns [year, month, day] parts
+const extractJpgDate = (sourcePath) => {
+    const buffer = readFileSync(sourcePath)
+    let tags = ExifReader.load(buffer);
+    return tags['DateTime'].value[0].substr(0, 10).split(':')
+}
+
+// Returns [year, month, day] parts
+const extractPngDate = (sourcePath) => {
+    const buffer = readFileSync(sourcePath)
+    let tags = ExifReader.load(buffer);
+    return tags['DateCreated'].value.substr(0, 10).split('-')
+}
+
 chokidar
     .watch(sourceRootPath, { ignoreInitial: true, usePolling: false, interval: 5000 })
     .on('add', async (sourcePath) => {
         console.log(`Detected new file...`)
         console.log(`> Source file ${sourcePath}`)
 
-        new ExifImage({ image: sourcePath}, async (err, data) => {
-            if (err) {
-                throw err
-            } else {
+        let date = null
+        switch (path.extname(sourcePath).toLowerCase()) {
+            case '.jpg':
+            case '.jpeg':
+                date = extractJpgDate(sourcePath)
+                break;
+            case '.png':
+                date = extractPngDate(sourcePath)
+                break;
+        }
 
-                const rawDate = data.exif["CreateDate"] || data.exif["DateTimeOriginal"]
+        if (date === null) {
+            console.log('! Unknown file type, skipping')
+        }
 
-                const filename = path.basename(sourcePath)
-                const [year, month, day] = rawDate.substr(0, 10).split(':')
+        const filename = path.basename(sourcePath)
+        const [year, month, day] = date
 
-                const destinationPath = path.join(destinationRootPath, year, month, day, filename)
-                await mkdir(path.dirname(destinationPath), { recursive: true})
+        const destinationPath = path.join(destinationRootPath, year, month, day, filename)
+        await mkdir(path.dirname(destinationPath), { recursive: true})
 
-                copyFileSync(sourcePath, destinationPath)
+        copyFileSync(sourcePath, destinationPath)
 
-                console.log(`> Copied to ${destinationPath}`)
-            }
-        })
+        console.log(`> Copied to ${destinationPath}`)
     });
