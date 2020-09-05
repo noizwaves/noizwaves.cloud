5d2b74cf8c30241a98f7e5718dc84a7ddcf21325
Adam Neumann <noizwaves@users.noreply.github.com>
2020-09-05 15:31:48 -0600

    Use duplicity instead of handrolled timestamps to do monthly backups

--- hot_backup.sh
+++ hot_backup.sh
@@ -6,24 +6,6 @@ source ~/cloud-config/hot.env
 DEST="s3://us-east-1.linodeobjects.com/$BUCKET_NAME/"
 CONTAINERS="resilio-sync standardnotes_sync standardnotes_mariadb freshrss freshrss_mariadb bitwarden"
 
-LAST_FULL_BACKUP_FILE=~/cloud-config/last-full-backup.txt
-LAST_FULL_BACKUP=0
-if [[ -f $LAST_FULL_BACKUP_FILE ]]; then
-  LAST_FULL_BACKUP="$(cat $LAST_FULL_BACKUP_FILE)"
-fi
-
-NOW=$(date +"%s")
-SECS_SINCE_FULL="$(($NOW-$LAST_FULL_BACKUP))"
-
-# first full backup in just under 4 weeks
-if [[ SECS_SINCE_FULL -ge "$((60 * 60 * 24 * 28 - 600))" ]]; then
-  COMMAND=full
-  # TODO: update $LAST_FULL_BACKUP_FILE only upon successful backup
-  echo "$NOW" > $LAST_FULL_BACKUP_FILE
-else
-  COMMAND=incr
-fi
-
 docker stop $CONTAINERS
 
 docker run --rm \
@@ -36,8 +18,9 @@ docker run --rm \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   -e PASSPHRASE="${PASSPHRASE}" \
   wernight/duplicity \
-  duplicity $COMMAND \
+  duplicity \
   --progress \
+  --full-if-older-than 1M \
   --exclude '/data/cloud-data/resilio-sync/data/storage-data/' \
   --exclude '/data/cloud-data/nextcloud/' \
   --exclude '/data/cloud-data/traefik/' \
