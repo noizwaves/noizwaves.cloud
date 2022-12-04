03cc61b867f9d703bfbe48fc03b3a4c95e175462
Adam Neumann <noizwaves@users.noreply.github.com>
2022-12-03 18:49:16 -0700

    Add restore helpers to cold_restore

--- cold_restore.sh
+++ cold_restore.sh
@@ -4,12 +4,16 @@ source cold.env
 
 mkdir -p "${RESTORE_DIR}"
 
+# To restore a specific directory, add:
+# --file-to-restore cloud-data/resilio-sync/data/zettlekasten-data/Daily \
+
 docker run \
 	--name duplicity-cold \
 	--hostname duplicity-cold \
 	--user 1000:1000 \
 	--rm \
 	-v /etc/localtime:/etc/localtime:ro \
+	-v ~/cloud-config/.duplicity-cache:/home/duplicity/.cache/duplicity:rw \
 	-v "${BACKUP_DIR}":/backup:ro \
 	-v "${RESTORE_DIR}":/restore:rw \
 	wernight/duplicity \
