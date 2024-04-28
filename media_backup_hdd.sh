9ff438ae4232715387a6813ad7f3095bf20fdf56
Adam Neumann <noizwaves@users.noreply.github.com>
2024-04-27 17:44:26 -0700

    Support unmounted devices

--- media_backup_hdd.sh
+++ media_backup_hdd.sh
@@ -2,14 +2,22 @@
 
 set -eou pipefail
 
-mkdir -p /media/ozo/backup/TV
-rsync -av /mnt/media2/TV/ /media/ozo/backup/TV
+if findmnt --mountpoint /media/ozo >>/dev/null; then
+  mkdir -p /media/ozo/backup/TV
+  rsync -av /mnt/media2/TV/ /media/ozo/backup/TV
+else
+  echo "ozo is not mounted, skipping"
+fi
 
-mkdir -p /media/thecup/backup/{Movies,Music,Games,Photography,Videos,Books}
-rsync -av /mnt/media2/Movies/ /media/thecup/backup/Movies
-rsync -av /mnt/media2/Music/ /media/thecup/backup/Music
-rsync -av /mnt/media2/Games/ /media/thecup/backup/Games
-rsync -av /mnt/media2/Photography/ /media/thecup/backup/Photography
-rsync -av /mnt/media2/Videos/ /media/thecup/backup/Videos
-rsync -av /mnt/media2/Books/ /media/thecup/backup/Books
+if findmnt --mountpoint /media/thecup >>/dev/null; then
+  mkdir -p /media/thecup/backup/{Movies,Music,Games,Photography,Videos,Books}
+  rsync -av /mnt/media2/Movies/ /media/thecup/backup/Movies
+  rsync -av /mnt/media2/Music/ /media/thecup/backup/Music
+  rsync -av /mnt/media2/Games/ /media/thecup/backup/Games
+  rsync -av /mnt/media2/Photography/ /media/thecup/backup/Photography
+  rsync -av /mnt/media2/Videos/ /media/thecup/backup/Videos
+  rsync -av /mnt/media2/Books/ /media/thecup/backup/Books
+else
+  echo "thecup is not mounted, skipping"
+fi
 
